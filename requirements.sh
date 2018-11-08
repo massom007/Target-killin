507e9565bbd2bc61dfe58c1676852dfdf9056f93
massom007 <44805842+massom007@users.noreply.github.com>
2018-11-08 12:27:48 +0500

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,19 @@
+pip2 install gem
+gem install lolcat
+
+echo"         Missing packajes installing plz wait " |  lolcat  -a -d 50 -s 13
+apt update
+apt upgrade
+pkg install python2
+pip2 install --upgrade pip
+pip2 install mechanize
+pkg install git
+chmod +x T-killing.py
+echo "        Now use Target Killing  by >> python2 T-killing.py <<" | lolcat
+
+echo "        if u have no passlist then just type >> password.txt in 2nd step <<" | lolcat
+sleep 14
+
+
+
+
